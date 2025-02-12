8d5823f3527c9941eea8a22ba966272be8e2fb04
Hugo Medina Rivas <60479842+HuGhOsTmR@users.noreply.github.com>
2025-02-12 17:35:02 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import PhotoFrame from './components/PhotoFrame';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
